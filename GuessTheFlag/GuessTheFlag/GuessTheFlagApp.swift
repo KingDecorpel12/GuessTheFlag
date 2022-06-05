c7a8bff46ffeea5cab603b73fda3a9ec9dbe2cc1
Omar Hegazy <48699321+KingDecorpel12@users.noreply.github.com>
2022-06-05 15:19:18 -0400

    Add files via upload

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Omar Hegazy on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
